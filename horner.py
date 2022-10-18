8c2de60191adbe816ba6356dfa882642e624dbc3
Shadmehr <56414145+shadmehr-salehi@users.noreply.github.com>
2022-10-18 22:27:50 +0330

    Add files via upload

---
+++ horner.py
@@ -0,0 +1,15 @@
+# 2 X^3 + 3 X^2 -4X + 1 = 1185
+
+CoeArr = [] 
+n = int(input("please enter number of Coeffisents \n"))
+for i in range(n):
+    Coe = int(input("please enter Coe {} \n".format(i)))
+    CoeArr.append(Coe)
+
+sum = CoeArr[0]
+x = int(input("please enter number X \n"))
+
+for i in range(1,n):
+    sum = x*sum + CoeArr[i]
+
+print(sum)
